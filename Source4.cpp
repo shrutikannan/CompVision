6e523e93578b6669940cedd072b5b79b19241f0a
shrutikannan <kshruti@bu.edu>
2017-10-03 06:46:48 -0400

    Update Source4.cpp
    Erosion and dilation added

--- Source4.cpp
+++ Source4.cpp
@@ -18,10 +18,10 @@ using namespace std;
 Mat3b out;
 Mat negate_pixel(Mat src);
 //void RecursiveConnectedComponents(Mat img_negate, int row, int col, int label);
-void StackConnectedComponents(Mat img_binary, Mat1i labels, Mat img_labelled);
+void StackConnectedComponents(Mat img_binary);
 void show_components(Mat img_negate);
 void img_erosion(Mat src, Mat dst);
-
+void img_dilation(Mat src, Mat dst);
 vector<Mat> neighbour_list;
 //void applyCustomColormap(const Mat1i& src, Mat3b& dst);
 Mat output;// = Mat::zeros(img.size(), CV_8UC3);
@@ -44,19 +44,25 @@ int main()
 	}
 	}
 	*/
-	Mat img = imread("open_fist-bw.png", IMREAD_GRAYSCALE);
+	Mat img = imread("tumor-fold.png", IMREAD_GRAYSCALE);
 	imshow("image", img);
 
-	Mat img_negate = negate_pixel(img);
-	imshow("Negated image", img_negate);
-	//	show_components(img_negate);
+	Mat img_negate1 = negate_pixel(img);
+	imshow("Negated image", img_negate1);
+
+	Mat img_negate = img_negate1.clone();
+	img_erosion(img_negate1, img_negate);
+	img_erosion(img_negate1, img_negate);
+	img_erosion(img_negate1, img_negate);
+	img_dilation(img_negate1, img_negate);
+	img_dilation(img_negate1, img_negate);
+	imshow("eroded", img_negate);
+	
 	Mat img_binary;
 	threshold(img_negate, img_binary, 0.0, 1.0, THRESH_BINARY);
 	imshow("Binary image", img_binary);
-	
-	Mat1i labels;
-	Mat img_labelled = img_binary.clone();
-	StackConnectedComponents(img_binary, labels, img_labelled);
+
+	StackConnectedComponents(img_binary);
 	
 	Mat output = Mat::zeros(img.size(), CV_8UC3);
 	for (size_t i = 0; i < blobs.size(); i++) { 	// Randomly color the blobs
@@ -71,47 +77,12 @@ int main()
 			output.at<Vec3b>(y, x)[2] = r;
 		}
 	}
-
 	imshow("labelled", output);
 	waitKey(0);
 	return 0;
 }
-/*
-void RecursiveConnectedComponents(Mat src, int row, int col, int label) {
-//	RecursiveConnectedComponents(pixel, label)
-//			for each unmarked neighbor
-//			assign the label to the neighbor
-//			call RecursiveConnectedComponents(neighbor, label)
-	
-	src.at<uchar>(row, col) = label;// assign label to current pixel
-	for (int dx = -1; dx <= 1; dx++) { // loop over neighbours
-		int *rowPtr = (int*)src.ptr(row);
-		for (int dy = -1; dy <= 1; dy++) {
-			if (rowPtr[col] == 1) { // for each neighbour pixel, check if label is -1 (part of object)
-				RecursiveConnectedComponents(src, row + dx, col + dy, label); // if yes, then change label for that too, and look among its neighbours
-			}
-		}
-	}
-	return;
-}
-*/
-
-/*
-StackConnectedComponents(label)
-declare a stack data structure
-push the first region pixel into the stack
-
-while (the stack is not empty) {
-	pop an item off the stack
-		for each unmarked neighbor{
-			assign the label to the neighbor
-			push the neighbor into the stack
-		}
-}*/
-
-void StackConnectedComponents(Mat img_binary, Mat1i labels, Mat img_labelled) {
+void StackConnectedComponents(Mat img_binary) {
 	Mat1b src = img_binary > 0;
-	labels = Mat1i(img_binary.rows, img_binary.cols, 0);
 
 	int label = 0;
 	int w = src.cols;
@@ -120,49 +91,39 @@ void StackConnectedComponents(Mat img_binary, Mat1i labels, Mat img_labelled) {
 
 	cv::Point point;
 	for (int row = 0; row<h; row++) {
-		int *rowP = (int*)labels.ptr(row);
 		for (int col = 0; col<w; col++) {
 			if ((src(row, col)) > 0) {   // Non zero element
-//			if (rowP[col] > 0) {
 				vector <Point2i> blob;
 				std::stack<int, std::vector<int>> stack2; // Declare a stack
 				i = col + row*w; // information about the current row and col is here
 				stack2.push(i); // store in the stack
-
 				std::vector<cv::Point> comp; // points belonging to 1 object stored here
 
-				while (!stack2.empty()) // while the stack is not empty this loop will go on
-				{
+				while (!stack2.empty()) {// while the stack is not empty this loop will go on
 					i = stack2.top(); // extract the first element
 					stack2.pop(); // remove the first element
 
 					int x2 = i%w; // col number of current elmt
 					int y2 = i / w; // row no of current elmt
 
-//					src(y2, x2) = 0; // set to 0
-					
-
+					src(y2, x2) = 0; // set to 0
 					point.x = x2; // creating a new point
 					point.y = y2;
 					comp.push_back(point); // storing that point in a vector
 
-					if (x2 > 0 && y2 > 0 && (src(y2 - 1, x2 - 1) != 0))
-					{
+					if (x2 > 0 && y2 > 0 && (src(y2 - 1, x2 - 1) != 0)) {
 						stack2.push(i - w - 1);
 						src(y2 - 1, x2 - 1) = 0;
 					}
-					if (x2 > 0 && y2 < h - 1 && (src(y2 + 1, x2 - 1) != 0))
-					{
+					if (x2 > 0 && y2 < h - 1 && (src(y2 + 1, x2 - 1) != 0)) {
 						stack2.push(i + w - 1);
 						src(y2 + 1, x2 - 1) = 0;
 					}
-					if (x2 < w - 1 && y2>0 && (src(y2 - 1, x2 + 1) != 0))
-					{
+					if (x2 < w - 1 && y2>0 && (src(y2 - 1, x2 + 1) != 0)) {
 						stack2.push(i - w + 1);
 						src(y2 - 1, x2 + 1) = 0;
 					}
-					if (x2 < w - 1 && y2 < h - 1 && (src(y2 + 1, x2 + 1) != 0))
-					{
+					if (x2 < w - 1 && y2 < h - 1 && (src(y2 + 1, x2 + 1) != 0)) {
 						stack2.push(i + w + 1);
 						src(y2 + 1, x2 + 1) = 0;
 					}
@@ -180,7 +141,6 @@ Mat negate_pixel(Mat src) {
 	for (int i = 0; i < src.rows; i++) {
 		for (int j = 0; j < src.cols; j++) {
 			if (src.at<uchar>(i, j) < 50) {
-				//			if(src.at<uchar>(i,j) == 1) {
 				dst.at<uchar>(i, j) = -1;
 			}
 			else {
@@ -198,12 +158,41 @@ void show_components(Mat img_negate) {
 		}
 	}
 }
-/*
+
 void img_erosion(Mat src, Mat dst) {
-	for (int row = 0;row < src.rows;row++) {
-		int *rowP = (int*)src.ptr(row);
-		for (int col = 0; col < src.col-1;col++) {
-			if (rowP[col] && rowP[col+1] && )
+	src = dst.clone();
+	dst = Mat::zeros(src.size(), CV_8UC1);
+	int count=0;
+	for (int i = 1; i < src.rows - 1; i++) {
+		for (int j = 1; j < src.cols - 1; j++) {
+			for (int dx = -1; dx <= 1; dx++) {
+				for (int dy = -1; dy <= 1; dy++) {
+					if (src.at<uchar>(i + dx, j + dy) != 0) {
+						count++;
+					}
+				}
+			}		
+		if (count == 9) dst.at<uchar>(i, j) = 255;
+		count = 0;
+		}
+	} 
+	return;
+}
+
+void img_dilation(Mat src, Mat dst) {
+	src = dst.clone();
+	dst = Mat::zeros(src.size(), CV_8UC1);
+	int count = 0;
+	for (int i = 1; i < src.rows - 1; i++) {
+		for (int j = 1; j < src.cols - 1; j++) {
+			if (src.at<uchar>(i, j) != 0) {
+				for (int dx = 0; dx <= 1; dx++) {
+					for (int dy = 0; dy <= 1; dy++) {
+						dst.at<uchar>(i + dx, j + dy) = 255;
+					}
+				}
+			}
 		}
 	}
-}*/
+	return;
+}
