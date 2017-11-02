6e8e9f1fe03be22dc2a34d4f4d29f463c89bac84
shrutikannan <kshruti@bu.edu>
2017-11-01 21:19:50 -0400

    Video creation added

--- bat_kf_vector.cpp
+++ bat_kf_vector.cpp
@@ -1,7 +1,9 @@
-///*
-//CS585 Image and Video Computing Fall 2017
-//Assignment 04
-//Authors: G Sivaperumal, Shruti Kannan, Prateek Mehta
+/*
+CS585 Image and Video Computing Fall 2017
+Assignment 04
+This code tracks objects in binary images
+Authors: G Sivaperumal, Shruti Kannan, Prateek Mehta
+*/
 
 #include "stdafx.h"
 #include "opencv2/core/core.hpp"
@@ -16,6 +18,7 @@
 #include <filesystem>
 #include <cmath>
 
+
 namespace fs = std::experimental::filesystem;
 using namespace cv;
 using namespace std;
@@ -29,34 +32,33 @@ void operations(Mat &output);
 
 int main() 
 {
-	// Finding no of files in Loc_files
+	//VideoWriter to create output video
+	VideoWriter video("bat_track.avi", CV_FOURCC('M', 'J', 'P', 'G'), 3, Size(1024, 1024));
+
+	// Storing file names of Segmentation and Localization in vectors
 	vector<string> filename_loc;
 	vector<string> filename_seg;
 	std::string path = "Localization_Bats/";
-	std::string path1 = "Seg_images/"; //1
-	const fs::directory_iterator end{}; //
+	std::string path1 = "Seg_images/"; // Binary images folder
+	const fs::directory_iterator end{}; 
 	int no_frames = 0;
 	for (fs::directory_iterator iter{ path }; iter != end; ++iter) {
-		filename_loc.push_back(iter->path().string());
+		filename_loc.push_back(iter->path().string()); // Contains Localization filenames
 		no_frames++;
 	}
 	for (fs::directory_iterator iter{ path1 }; iter != end; ++iter) {
-		filename_seg.push_back(iter->path().string());
+		filename_seg.push_back(iter->path().string()); // Contains Segmentation filenames
 	}
 
-//	for (int i = 0; i < filename_seg.size(); i++)
-//		cout << filename_seg[i] << endl;
-
-	// Init Kalman parameters
+	// Create vector of KF objects and Initialize Kalman parameters
 	vector <KalmanFilter> kf_vector;
 	for (int i = 0; i < no_frames; i++) {
 		KalmanFilter KF(2, 2, 0);
 		init_kalman(KF);
-		kf_vector.push_back(KF); // state dim = 2, measurement dim = 2
+		kf_vector.push_back(KF); // State dim = 2, Measurement dim = 2
 	}
 
-	// Loop through Loc_files and create vector
-	vector <vector <int>> file_data, file_data1, file_data2;
+	vector <vector <int>> file_data, file_data1, file_data2; // Vectors to store centers of current and next frame
 
 	// Declarations
 	int tmp = 0, tmp1 = 0;
@@ -69,94 +71,86 @@ int main()
 	std::vector<cv::Point> blob0, blob;
 	int index = 0;
 	vector <vector <double>> colors;
-
-	file_processing(filename_loc[0], file_data);
 	vector <double> c;
 
-	c.clear();
+	file_processing(filename_loc[0], file_data); // Initialising frame#0 Localization file to vector of centers
+	
+	c.clear(); // Vector for colours
 	for (int i = 0; i < file_data.size(); i++)
 	{
+		c.push_back(255 * (rand() / (1.0 + RAND_MAX))); // Creating random colors and storing in vector
 		c.push_back(255 * (rand() / (1.0 + RAND_MAX)));
 		c.push_back(255 * (rand() / (1.0 + RAND_MAX)));
-		c.push_back(255 * (rand() / (1.0 + RAND_MAX)));
-		cout << "intial_color_allocation:" << c.at(0) << " " << c.at(1) << " " << c.at(2) << " " << endl;
-		//cout << "intial_color_values:" << a << " " << b << " " << d << " " << endl;
+		// cout << "intial_color_allocation:" << c.at(0) << " " << c.at(1) << " " << c.at(2) << " " << endl;
 		colors.push_back(c);
 		c.clear();
 	}
 	
-	for (int current_loc = 0; current_loc < no_frames - 1; current_loc++) 
+	for (int current_loc = 0; current_loc < no_frames - 1; current_loc++)  // Loop over Localization files
 	{
-		//		if (tmp > 5) break;
-		tmp++;
-		cout << "Frame no: " << tmp << endl;
-
-		// Create vector of center coordinates for current and next frame
-		file_data1.clear(); // 2
-		file_processing(filename_loc[current_loc + 1], file_data1);
-		cout << "  Size of file_data: " << file_data.size() << endl;
-		cout << "  Size of file_data1: " << file_data1.size() << endl;
+		// Convert Localization file (with centers) to vector
+		file_data1.clear();
+		file_processing(filename_loc[current_loc + 1], file_data1); // Next frame Localization file to vector of centers
 
 		prediction = kf_vector[current_loc].predict();
 		predictPt.x = prediction.at<float>(0);
 		predictPt.y = prediction.at<float>(1);
 
-		//convertFileToMat(filename_seg[current_loc], labelled0, binary0);
-		//convertFileToMat(filename_seg[current_loc + 1], labelled, binary);
-		binary0 = imread(filename_seg[current_loc], CV_LOAD_IMAGE_GRAYSCALE); //5
-		binary = imread(filename_seg[current_loc + 1], CV_LOAD_IMAGE_GRAYSCALE); //6
-		cvtColor(binary0, output0, CV_GRAY2BGR);
+		binary0 = imread(filename_seg[current_loc], CV_LOAD_IMAGE_GRAYSCALE); // Reading the binary images
+		binary = imread(filename_seg[current_loc + 1], CV_LOAD_IMAGE_GRAYSCALE); 
+		cvtColor(binary0, output0, CV_GRAY2BGR); // Copying the binary to output images
 		cvtColor(binary, output, CV_GRAY2BGR);
 
 		//operations(output); // if video is reversed (1/3)
 		//operations(output0); // if video is reversed (2/3)
 
-		for (int i = 0; i < file_data.size(); i++)
-		{ // Loop over bats in 1 loc file
-													 // cout << "  Bat no: " << i+1 << endl;
-
-													 //			if (tmp1 > 5) break;
+		for (int i = 0; i < file_data.size(); i++) // Loop over bats in each Localization file
+		{ 
 			tmp1++;
 			measurement(0) = file_data[i][0];
 			measurement(1) = file_data[i][1];
 			current_bat.push_back(file_data[i][0]);
 			current_bat.push_back(file_data[i][1]);
-			//			cout << "measurement: " << measurement(0) << "," << measurement(1) << endl;
+			// cout << "measurement: " << measurement(0) << "," << measurement(1) << endl;
 
-			estimation = kf_vector[current_loc].correct(measurement);
-			estimated_bat.x = estimation.at<float>(0);
-			estimated_bat.y = estimation.at<float>(1); // Estimation of KF
-													   //			cout << "estimated: " << estimated_bat.x << "," << estimated_bat.y << endl;
+			estimation = kf_vector[current_loc].correct(measurement); // Get estimate for the bat in next frame
+			estimated_bat.x = estimation.at<float>(0); // Estimation of KF as a Point
+			estimated_bat.y = estimation.at<float>(1);
+			// cout << "estimation: " << estimated_bat.x << "," << estimated_bat.y << endl;
 
-			get_next_center(estimated_bat, predicted_bat, file_data1, index); // Likely next bat
-																			  //			cout << "MF predicted Bat: " << predicted_bat[0] << "," << predicted_bat[1] << endl;
-			file_data1.erase(file_data1.begin() + index);
+			get_next_center(estimated_bat, predicted_bat, file_data1, index); // Likely next bat center
+			// cout << "MF predicted Bat: " << predicted_bat[0] << "," << predicted_bat[1] << endl;
+
+			file_data1.erase(file_data1.begin() + index); // Remove bat from this vector to check if extra bats exist
 			file_data2.push_back(predicted_bat);
 			current_bat.clear();
-			if (file_data1.size() == 0) {
+			if (file_data1.size() == 0) { // If next frame has lesser bats than previous frame then break loop
 				cout << "     Next frame has lesser bats!" << endl;
 				break;
 			}
 		}
 
-		file_data2.insert(std::end(file_data2), std::begin(file_data1), std::end(file_data1));
-		//CvPoint* points = new CvPoint[2];
-		cout << "  Size of file_data2: " << file_data2.size() << endl;
-		color_bat(file_data, file_data2,colors, output, kf_vector);
+		file_data2.insert(std::end(file_data2), std::begin(file_data1), std::end(file_data1)); // Insert extra bats to this vector
+		color_bat(file_data, file_data2,colors, output, kf_vector); // Colour the bats
 		cv::namedWindow("output current frame", WINDOW_NORMAL);
 		cv::imshow("output current frame", output0);
 		cv::namedWindow("output next frame", WINDOW_NORMAL);
 		cv::imshow("output next frame", output);
 		file_data = file_data2;
 		file_data2.clear();
-		char key = waitKey(0);
-	}
+		
+		//write the output
+//		if (current_loc == 0)
+//			video.write(output0);
+		video.write(output);
 
+		//char key = waitKey(0);
+	}
+	video.release();
 	return 0;
 }
 
-void operations(Mat &output) {
-	//cv::rotate(output, output, ROTATE_180);
+void operations(Mat &output) {  // In case frames do not appear properly
 	transpose(output, output);
 	flip(output, output, 1);
 	transpose(output, output);
@@ -173,7 +167,6 @@ void color_bat(vector<vector<int>>current_frame, vector<vector<int>>next_frame, 
 	int bat_no_diff = next_frame.size() - current_frame.size();
 	cout << "difference in bats:" << bat_no_diff << endl;
 	vector<double> single_pixel;
-	//cout << single_pixel.at(0) <<single_pixel.at(1)<< single_pixel.at(2) << endl;
 
 	cout << "colors_size" << colors.size() << endl;
 	for (int i = 0; i < current_frame.size(); i++)
@@ -185,16 +178,10 @@ void color_bat(vector<vector<int>>current_frame, vector<vector<int>>next_frame, 
 		{
 			single_pixel = colors.at(i);
 			cout << "i: " << i << endl;
-			//cout << single_pixel.at(0) << single_pixel.at(1) << single_pixel.at(2) << endl;
 			line(output, Point(current_frame[i][0], current_frame[i][1]), Point(next_frame[i][0], next_frame[i][1]),
 				Scalar(single_pixel.at(0), single_pixel.at(1), single_pixel.at(2)), 2, LINE_8);
 			single_pixel.clear();
 		}
-		//catch (...)
-		//{
-		//	cout << "In catch" << i << endl;
-		//	continue;
-		//}
 	}
 	if (bat_no_diff > 0)
 		for (int i = 0; i < bat_no_diff;i++)
@@ -210,25 +197,6 @@ void color_bat(vector<vector<int>>current_frame, vector<vector<int>>next_frame, 
 			init_kalman(KF);
 			kf_vector.push_back(KF);
 		}
-	
-
-
-	//	Point center(predicted_bat[1], predicted_bat[0]); // if video is reversed (3/3)
-	//circle(output, center, 3, Scalar(0, 0, 255), -1, 8);
-
-	/*
-	StackConnectedComponents(binary, predicted_bat, blob, src);
-	cout << "Blob size: " << blob.size() << endl;
-	for (int j = 0; j < blob.size(); j++) {
-	Vec3b color;
-	color[0] = 0;
-	color[1] = 0;
-	color[2] = 255;
-	output.at<Vec3b>(Point(blob[j].y, blob[j].x)) = color;
-	//cout << "binary values: " << (int)binary.at<uchar>(blob[j].y, blob[j].x) << endl;
-	}
-	//cout << "binary values zeros: " << (int)binary.at<uchar>(0, 0) << endl;
-	*/
 }
 
 void convertFileToMat(String filename, Mat& labelled, Mat& binary) {
@@ -273,11 +241,9 @@ void get_next_center(Point &estimated_bat, vector <int> &predicted_bat, vector <
 	float difference, distance = 0;
 	for (int i = 0; i < file_data1.size(); i++) { // Loop through centers of next file
 		Point nfp(file_data1[i][0], file_data1[i][1]);
-		//		cout << "i=" << i << "nfp=" << nfp.x << "," << nfp.y << " ";
 		if (i == 0) {
-			difference = sqrt(pow((estimated_bat.x - nfp.x), 2) + pow((estimated_bat.y - nfp.y), 2));
-			index = 0;
-			// compute distance between bats
+			difference = sqrt(pow((estimated_bat.x - nfp.x), 2) + pow((estimated_bat.y - nfp.y), 2)); // compute distance between bats
+			index = 0;			
 		}
 		else {
 			distance = sqrt(pow((estimated_bat.x - nfp.x), 2) + pow((estimated_bat.y - nfp.y), 2));
@@ -286,9 +252,7 @@ void get_next_center(Point &estimated_bat, vector <int> &predicted_bat, vector <
 				index = i;
 			}
 		}
-		//		cout << "difference=" << difference << endl;
 	}
-	//cout << "index" << index << endl;
 	predicted_bat = file_data1[index];
 }
 
@@ -326,14 +290,3 @@ void file_processing(string filename_det, vector <vector<int>>& data_store) {
 		data_store.push_back(datarow);
 	}
 }
-
-/*
-for (int i = 0; i < file_data1.size(); i++)
-cout << file_data1[i][0] << " " << file_data1[i][1] << endl;
-//output0 = Mat::zeros(binary0.size(), CV_8UC3);
-//output = Mat::zeros(binary.size(), CV_8UC3);
-//namedWindow("binary current image", WINDOW_NORMAL);
-//imshow("binary current image", binary0);
-//namedWindow("binary next image", WINDOW_NORMAL);
-//imshow("binary next image", binary);
-*/
